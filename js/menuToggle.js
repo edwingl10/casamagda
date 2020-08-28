e1df33f963d168d2ce67f34aaf48107c5377f0ec
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-08-28 08:23:46 -0700

    navbar animation, styling
    - hamburger menu animation when clicked
    - links animation

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -9,17 +9,14 @@ navBarToggle.addEventListener('click', ()=>{
 }); 
 */
 
-const hamMenu = document.querySelector(".hamburger");
-const hamLines = document.querySelector(".ham-lines");
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu li");
 
-const navLinks = document.getElementById('links');
-let navClosed = true;
-
-hamMenu.addEventListener("click", ()=>{
-    if(navClosed){
-        hamLines.classList.toggle("rotate-ham-menu");
-    }
-    
-    navLinks.style.display = 'flex';
-    navClosed = !navClosed;
-})
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("rotate-ham-menu");
+    menu.classList.toggle("open");
+    links.forEach(link =>{
+        link.classList.toggle("fade");
+    });
+});
