0913a7538a4954c65977a1c8f0c0243731ba707a
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-08-29 23:29:18 -0700

    navbar bug fixes
    - fixed bug where couldnt see close option
    - when closing menu, determines if should show sticky nav

--- js/menu.js
+++ js/menu.js
@@ -3,26 +3,53 @@ const nav = document.querySelector("nav");
 const header = document.querySelector(".header");
 
 const hamIcon = document.querySelector(".js-hamburger");
+let closed = true;
 
 //sticky navigation
-var waypoint = new Waypoint({
+var aboutSection = new Waypoint({
     element: document.querySelector('.js--section-about'),
     handler: function(direction) {
-      
-      if(direction == "down"){
-          header.classList.add("fixed");
-          hamIcon.classList.add("black");
-      }
-      else{
-          header.classList.remove("fixed");
-          hamIcon.classList.remove("black");
-      }
+        //if the window is closed
+        if(closed){
+            if(direction == "down"){
+                header.classList.add("fixed");
+                hamIcon.classList.add("black");
+            }
+            else{
+                header.classList.remove("fixed");
+                hamIcon.classList.remove("black");
+            }
+        }
     },
     offset: '60px'
 });
 
 //shows nav elements 
 hamMenu.addEventListener("click", ()=>{
+    closed = !closed;
+    //makes ham icon black
+    hamIcon.classList.add("black");
+    //shows links
     nav.classList.toggle("active");
+
     hamMenu.classList.toggle("rotate-ham-menu");
+    //makes header fixed so can see x throughout menu
+    header.classList.add("fixed");
+    
+    //if the ilnks window were closed
+    if(closed)
+        changeHamColor(window.pageYOffset, aboutSection.triggerPoint);
+
 });
+
+//determines if nav should be stciky
+const changeHamColor = (win, wp) => {
+
+    if(win < wp){
+        header.classList.remove("fixed");
+        hamIcon.classList.remove("black");
+    }else{
+        header.classList.add("fixed");
+        hamIcon.classList.add("black");
+    }
+}
