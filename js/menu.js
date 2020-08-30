354318426b7222864321a3cd85fa1fc62c0b3680
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-08-29 21:13:26 -0700

    navbar content
    - navbar content sticks to full screen even when scrolling

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,20 @@ const hamMenu = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
 const header = document.querySelector(".header");
 
+const hamIcon = document.querySelector(".js-hamburger");
+
 //sticky navigation
 var waypoint = new Waypoint({
     element: document.querySelector('.js--section-about'),
     handler: function(direction) {
+      
       if(direction == "down"){
           header.classList.add("fixed");
+          hamIcon.classList.add("black");
       }
       else{
           header.classList.remove("fixed");
+          hamIcon.classList.remove("black");
       }
     },
     offset: '60px'
@@ -19,8 +24,5 @@ var waypoint = new Waypoint({
 //shows nav elements 
 hamMenu.addEventListener("click", ()=>{
     nav.classList.toggle("active");
-    //scroll
-    document.body.classList.toggle("no-scroll");
-    //hamburger menu animation
     hamMenu.classList.toggle("rotate-ham-menu");
 });
