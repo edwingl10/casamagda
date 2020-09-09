8b72348d8718ae56ea39cbe345005bd23c0faf8b
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-09-09 09:00:38 -0700

    nav/footer scroll
    - added nav menu scroll
    - added footer items scroll

--- js/menu.js
+++ js/menu.js
@@ -57,3 +57,31 @@ window.addEventListener("resize", ()=>{
         clicked();
     }
 })
+
+$(document).ready(function(){
+    //call to action btn scroll
+    $('.js--learn-more').click(function(){
+        $('html,body').animate({scrollTop: ($('.js--section-about').offset().top) - 60}, 1000);
+    });
+
+    //Navigation scroll
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+        // On-page links
+        if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            if(!closed){
+                clicked();
+            }
+            $('html, body').animate({scrollTop: (target.offset().top) - 60}, 1000);
+        }
+        }
+    });
+
+});
